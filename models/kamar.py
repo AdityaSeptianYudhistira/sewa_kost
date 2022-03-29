451446e0389711a55ef0062852fee02c42744d4a
Aditya Septian Yudhistira <aditya.septian.yudhistira@hashmicro.com>
2022-03-29 07:10:20 +0700

    test-6

--- models/kamar.py
+++ models/kamar.py
@@ -18,9 +18,9 @@ class Kamar(models.Model):
         string='Furnitur Tambahan',
         required=True
         )
-    orderkamardetail_ids = fields.One2many(
+    orderdetail_ids = fields.One2many(
         string='Order Detail',
-        comodel_name='kost.order_kamar_detail',
+        comodel_name='kost.order_detail',
         inverse_name='kamar_id',
     )
     harga = fields.Char(
--- models/order.py
+++ models/order.py
@@ -5,16 +5,10 @@ class Order(models.Model):
     _name = 'kost.order'
     _description = 'New Description'
 
-    orderkamardetail_ids = fields.One2many(
-        string='Detail Kamar',
-        comodel_name='kost.order_kamar_detail',
+    orderdetail_ids = fields.One2many(
+        string='Order Detail',
+        comodel_name='kost.order_detail',
         inverse_name='order_id',
-    )
-
-    orderpelayanandetail_ids = fields.One2many(
-        string='Detail Pelayanan',
-        comodel_name='kost.order_pelayanan_detail',
-        inverse_name='orderp_id',
     )
     
     name = fields.Char(string='Kode Order', required=True)
@@ -23,17 +17,16 @@ class Order(models.Model):
     total = fields.Char(compute='_compute_total', string='Total Harga', store=True)
 
     
-    @api.depends('orderkamardetail_ids')
+    @api.depends('orderdetail_ids')
     def _compute_total(self):
         for record in self:
             # Mengambil harga dari order detail ke dalam bentuk array kemudian dijumlahkan menggunakan sum()
-            sum_a = sum(self.env['kost.order_kamar_detail'].search([('order_id', '=', record.id)]).mapped('harga'))
-            sum_b = sum(self.env['kost.order_pelayanan_detail'].search([('order_id', '=', record.id)]).mapped('harga'))
-            record.total = sum_a + sum_b
+            sum_harga = sum(self.env['kost.order_detail'].search([('order_id', '=', record.id)]).mapped('harga'))
+            record.total = sum_harga
 
 
-class OrderKamarDetail(models.Model):
-    _name = 'kost.order_kamar_detail'
+class OrderDetail(models.Model):
+    _name = 'kost.order_detail'
     _description = 'New Description'
 
     order_id = fields.Many2one('kost.order', string='Order')
@@ -60,40 +53,9 @@ class OrderKamarDetail(models.Model):
     # Mengurangi stok berdasarkan qty
     @api.model
     def create(self, vals):
-        record = super(OrderKamarDetail, self).create(vals)
+        record = super(OrderDetail, self).create(vals)
         if record.qty:
             self.env['kost.kamar'].search([('id', '=', record.kamar_id.id)]).write({'stok': record.kamar_id.stok - record.qty})
             return record
 
-class OrderPelayananDetail(models.Model):
-    _name = 'kost.order_pelayanan_detail'
-    _description = 'New Description'
 
-    orderp_id = fields.Many2one('kost.order', string='Order')
-    pelayanan_id = fields.Many2one('kost.pelayanan', string='Pelayanan')
-
-    name = fields.Selection(
-        string='Name', 
-        selection=[('kamar', 'Kamar'), 
-                  ('pelayanan', 'Pelayanan')])
-    harga = fields.Integer(compute='_compute_harga', string='harga')
-    qty = fields.Integer(string='Lama Sewa')
-    harga_satuan = fields.Integer(compute='_compute_harga_satuan', string='Harga Perbulan')
-    
-    @api.depends('pelayanan_id')
-    def _compute_harga_satuan(self):
-        for record in self:
-            record.harga_satuan = record.pelayanan_id.harga
-    
-    @api.depends('qty', 'harga_satuan')
-    def _compute_harga(self):
-        for record in self:
-            record.harga = record.harga_satuan * record.qty
-    
-    # Mengurangi stok berdasarkan qty
-    @api.model
-    def create(self, vals):
-        record = super(OrderPelayananDetail, self).create(vals)
-        if record.qty:
-            self.env['kost.pelayanan'].search([('id', '=', record.pelayanan_id.id)]).write({'stok': record.pelayanan_id.stok - record.qty})
-            return record
